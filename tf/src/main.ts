083a72157657762fefd35f0d4103f8e362398c55
sebastian mendez <103006900+sebaditas@users.noreply.github.com>
2024-05-04 00:27:22 -0500

    Delete tf directory

--- tf/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-// This is the entry point for the application.
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
